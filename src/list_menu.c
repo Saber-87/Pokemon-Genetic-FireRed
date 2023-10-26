07ebf1c3242ebc0a0dad75720b475159e91d5417
Saber-87 <95220206+Saber-87@users.noreply.github.com>
2023-10-26 09:02:20 +0200

    Fairy Type + Icon + Pokemon Changes

--- src/list_menu.c
+++ src/list_menu.c
@@ -70,11 +70,13 @@ static const struct MoveMenuInfoIcon sMenuInfoIcons[] =
     [TYPE_ICE + 1]      = { 32, 12, 0x4C },
     [TYPE_DRAGON + 1]   = { 32, 12, 0xA0 },
     [TYPE_DARK + 1]     = { 32, 12, 0x8C },
+    [TYPE_FAIRY + 1]     = { 32, 12, 0x100},
     [MENU_INFO_ICON_TYPE]      = { 40, 12, 0xA8 },
     [MENU_INFO_ICON_POWER]     = { 40, 12, 0xC0 },
     [MENU_INFO_ICON_ACCURACY]  = { 40, 12, 0xC8 },
     [MENU_INFO_ICON_PP]        = { 40, 12, 0xE0 },
     [MENU_INFO_ICON_EFFECT]    = { 40, 12, 0xE8 },
+
 };
 
 static void ListMenuDummyTask(u8 taskId)
